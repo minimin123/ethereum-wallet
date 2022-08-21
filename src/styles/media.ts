d7d1a2e9e8bffaee1a4624feeda19378a692832a
minimin123 <kmmin727@gmail.com>
2022-08-21 20:19:56 +0900

    [style] media quuery setting 수정

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,4 +1,6 @@
 export const media = {
+  PC: '(min-width: 769px)',
   TABLET: '(min-width: 576px) and (max-width: 768px)',
   MOBILE: '(max-width: 575px)',
+  SMALL_MOBILE: '(max-width: 280px)',
 };
