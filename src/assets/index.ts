f5034a96efa424cfdfc34faee3659b630197b561
minimin123 <kmmin727@gmail.com>
2022-08-21 13:43:59 +0900

    [setting] assets 추가, color 설정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,4 @@
-export { ReactComponent as SearchIcon } from './svgs/metamask-fox.svg';
+export { ReactComponent as Logo } from './svgs/logo.svg';
+export { ReactComponent as FoxIcon } from './svgs/metamask-fox.svg';
+export { ReactComponent as ArrowIcon } from './svgs/icon-arrow.svg';
+export { ReactComponent as UserDefaultImg } from './svgs/user-default.svg';
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,8 @@
-export const color = {};
+export const color = {
+  WHITE: '#ffffff',
+  GRAY01: '#bbc0c5', // border
+  GRAY02: '#f2f4f6;', // back-ground
+  BLACK: '#24272a;',
+  BLUE: '#037dd6',
+  MAINNET: '#29b6af',
+};
