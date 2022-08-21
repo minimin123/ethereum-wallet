3cc44f395afaa001f9a08d1c10463df403d25875
minimin123 <kmmin727@gmail.com>
2022-08-21 14:19:48 +0900

    [setting] router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { Layout } from 'components/common/layout/layout';
+import MainPage from 'components/main/main';
+import SwapPage from 'components/swap/swap';
+
+import GlobalStyles from 'styles/globalStyles';
+
 const App = () => {
-  return <h1>test</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="swap" element={<SwapPage />} />
+            <Route path="*" element={<div>404</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
